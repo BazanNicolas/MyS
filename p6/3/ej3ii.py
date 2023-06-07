eead26efcb9b4e72c8484a109421a844ae36f6b8
Nicolás Bazán <bazan.nicolas.m@gmail.com>
2023-06-07 12:45:38 -0300

    added table for p6 ej 3ii

--- p6/3/ej3ii.py
+++ p6/3/ej3ii.py
@@ -1,5 +1,6 @@
 from math import sqrt, sin, pi
 from random import random
+from tabulate import tabulate
 
 def g(x):
     return 3/(3 + x**4)
@@ -18,8 +19,40 @@ def Media_Muestral_X(z_alfa_2, L): #z_alfa_2 = z_(alfa/2)
         MediaAnt = Media
         Media = MediaAnt + (X - MediaAnt) / n
         Scuad = Scuad * (1 - 1 /(n-1)) + n*(Media - MediaAnt)**2
-    return Media, n
+    return Media, n - 1, sqrt(Scuad)
 
 res = Media_Muestral_X(1.96, 0.002)
-print(f"La media muestral es: {res[0]}")
-print(f"El número de datos generados efectivamente es: {res[1]}")
+
+def incisoC(nSim, z_alfa_2, L):
+    'Confianza = (1 - alfa)%, amplitud del intervalo: L'
+    d = L / (2 * z_alfa_2)
+    rand = random()
+    Media = (1/rand**2) * g(1/rand - 1)    
+    Scuad, n = 0, 1
+    for _ in range(nSim):
+        n += 1
+        rand = random()
+        X = (1/rand**2) * g(1/rand - 1)            
+        MediaAnt = Media
+        Media = MediaAnt + (X - MediaAnt) / n
+        Scuad = Scuad * (1 - 1 /(n-1)) + n*(Media - MediaAnt)**2
+    return Media, sqrt(Scuad), n - 1
+
+res = Media_Muestral_X(1.96, 0.001)
+res1000 = incisoC(1000, 1.96, 0.002)
+res5000 = incisoC(5000, 1.96, 0.002)
+res7000 = incisoC(7000, 1.96, 0.002)
+table = [
+    ["1000", res1000[0], res1000[1], f"({res1000[0] - 1.96 * res1000[1]/sqrt(res1000[2])}, {res1000[0] + 1.96 * res1000[1]/sqrt(res1000[2])})"],
+    ["5000", res5000[0], res5000[1], f"({res5000[0] - 1.96 * res5000[1]/sqrt(res5000[2])}, {res5000[0] + 1.96 * res5000[1]/sqrt(res5000[2])})"],
+    ["7000", res7000[0], res7000[1], f"({res7000[0] - 1.96 * res7000[1]/sqrt(res7000[2])}, {res7000[0] + 1.96 * res7000[1]/sqrt(res7000[2])})"],
+    [f"{res[1]}", res[0], res[2], f"({res[0] - 1.96 * res[2]/sqrt(res[1])}, {res[0] + 1.96 * res[2]/sqrt(res[1])})"]
+]
+headers = ["nSim", "Mean", "Desv", "Intervalo de confianza"]
+print(tabulate(table, headers, tablefmt="fancy_grid"))
+
+
+# Nota: Niveles de confianza:
+# 90% -> 1.645
+# 95% -> 1.96
+# 99% -> 2.576
