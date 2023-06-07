ff5b71043e2747f78b660c0a2c3452a6a124c5a1
Nicolás Bazán <bazan.nicolas.m@gmail.com>
2023-06-07 12:53:54 -0300

    fix bug

--- p6/4/ej4c.py
+++ p6/4/ej4c.py
@@ -21,6 +21,21 @@ def Media_Muestral_X(z_alfa_2, L): #z_alfa_2 = z_(alfa/2)
         MediaAnt = Media
         Media = MediaAnt + (X - MediaAnt) / n
         Scuad = Scuad * (1 - 1 /(n-1)) + n*(Media - MediaAnt)**2
+    return Media
+
+def Varianza(z_alfa_2, L, nSim): #z_alfa_2 = z_(alfa/2)
+    'Confianza = (1 - alfa)%, amplitud del intervalo: L'
+    d = L / (2* z_alfa_2)
+    Media = N()
+    Scuad, n = 0, 1
+    for _ in range(nSim):
+        n += 1
+        X = N()
+        MediaAnt = Media
+        Media = MediaAnt + (X - MediaAnt) / n
+        Scuad = Scuad * (1 - 1 /(n-1)) + n*(Media - MediaAnt)**2
     return Scuad
 
-print(Media_Muestral_X(1.96, 0.025))
+print("estimación de e a través de la media: ", Media_Muestral_X(1.96, 0.025))
+# Var(N*) = Var(N)/n
+print("Varianza muestral de N* correspondiente a 1000 ejecuciones: ", Varianza(1.96, 0.025, 1000)/1000)
