e3d0fe41c4862787a31c3caec0f5d62eaa924619
Nicolás Bazán <bazan.nicolas.m@gmail.com>
2023-06-14 21:38:15 -0300

    review p7

--- p7/10/ej10.py
+++ p7/10/ej10.py
@@ -7,9 +7,9 @@ from scipy.stats import norm
 import numpy as np
 from scipy.stats import norm
 
-def normal(x, media, desvio):
-    valor_ajustado = (x - media) / desvio
-    return norm.cdf(valor_ajustado)
+# def normal(x, media, desvio):
+#     valor_ajustado = (x - media) / desvio
+#     return norm.cdf(valor_ajustado)
 
 def KolmogorovSmirnov(datos, media, desvio):
     n = len(datos)
@@ -23,16 +23,20 @@ def KolmogorovSmirnov(datos, media, desvio):
 def ej10():
     datos = [91.9, 97.8, 111.4, 122.3, 105.4, 95.0, 103.8, 99.6, 96.6, 119.3, 104.8, 101.7]
     n = len(datos)
-    media0 = sum(datos) / n
-    varianza0 = sum((x - media0)**2 for x in datos) / (n-1)
-    desv0 = varianza0 ** 0.5
+    media0 = np.mean(datos)
+    desv0 = np.std(datos)
+    # media0 = sum(datos) / n
+    # varianza0 = sum((x - media0)**2 for x in datos) / (n-1)
+    # desv0 = varianza0 ** 0.5
     d = KolmogorovSmirnov(datos, media0, desv0)
     pvalor = 0
     for _ in range(1000):
-        muestra = np.random.normal(media0, varianza0, n)
-        media = sum(muestra) / n
-        varianza = sum((x - media)**2 for x in datos) / n-1
-        desv = varianza ** 0.5
+        muestra = np.random.normal(media0, desv0, n)
+        # media = sum(muestra) / n
+        # varianza = sum((x - media)**2 for x in muestra) / (n-1)
+        # desv = varianza ** 0.5
+        media = np.mean(muestra)
+        desv = np.std(muestra)
         D = KolmogorovSmirnov(muestra, media, desv)
         if D >= d:
             pvalor += 1
